5c5b3e71a28c4811107e3b96b2320bff9e4e0196
samuelburnham <samuel.burnham@gmail.com>
2021-08-13 13:41:47 -0400

    Initial form setup

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -12,51 +12,43 @@ export function Main (props) {
   const [status, setStatus] = useState('');
 
   // The currently stored values
-  const [cid, setCid] = useState(0);
-  const [data, setData] = useState(0);
+  const [vec, setVec] = useState('');
+  // const [data, setData] = useState(0);
 
-  const [currentValue, setCurrentValue] = useState('');
+  // const [currentValue, setCurrentValue] = useState('');
   const [formValue, setFormValue] = useState('');
 
   useEffect(() => {
     let unsubscribe;
-      api.query.templateModule.cid(newValue => {
+    api.query.templateModule.vec(newValue => {
       // The storage value is an Option
       // So we have to check whether it is None first
       // There is also unwrapOr
-          if (newValue.isNone) {
-              setCurrentValue('<None>');
-          } else {
-              setCurrentValue(newValue.unwrap().toString());
-              let newVal = newValue.unwrap();
-              setCid(newVal[0].toString());
-              setData(newVal[1].toNumber());
-          }
-      }).then(unsub => {
-          unsubscribe = unsub;
+      if (newValue.isNone) {
+        setVec('<None>');
+      } else {
+        setVec(newValue.unwrap().toString());
+      }
+    }).then(unsub => {
+      unsubscribe = unsub;
     })
-          .catch(console.error);
+      .catch(console.error);
 
-      return () => unsubscribe && unsubscribe();
+    return () => unsubscribe && unsubscribe();
   }, [api.query.templateModule]);
 
   return (
     <Grid.Column width={20} style={{ textAlign: 'center' }}>
-      <h1>IPLD Storage</h1>
+      <h1>Vector Storage</h1>
         <Statistic
-          label='Currently Stored Data'
-          value={data}
-          size='mini'
-        />
-        <Statistic
-          label='Currently Stored CID'
-          value={cid}
+          label='Currently Stored Vector'
+          value={vec}
           size='mini'
         />
       <Form>
         <Form.Field>
           <Input
-            label='IPLD Input'
+            label='Vector Input'
             type='string'
             onChange={(_, { value }) => setFormValue(value)}
           />
@@ -64,24 +56,36 @@ export function Main (props) {
         <Form.Field style={{ textAlign: 'center' }}>
           <TxButton
             accountPair={accountPair}
-            label='Store IPLD data'
+            label='Store Vector'
             type='SIGNED-TX'
             setStatus={setStatus}
             attrs={{
               palletRpc: 'templateModule',
-              callable: 'storeIpld',
+              callable: 'storeVector',
               inputParams: [formValue],
               paramFields: [true]
             }}
           />
           <TxButton
             accountPair={accountPair}
-            label='Get IPLD data'
+            label='Get Vector'
             type='SIGNED-TX'
             setStatus={setStatus}
             attrs={{
               palletRpc: 'templateModule',
-              callable: 'retrieveIpld',
+              callable: 'retrieveVector',
+              inputParams: [formValue],
+              paramFields: [true]
+            }}
+          />
+          <TxButton
+            accountPair={accountPair}
+            label='Push front'
+            type='SIGNED-TX'
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: 'templateModule',
+              callable: 'pushFrontVector',
               inputParams: [formValue],
               paramFields: [true]
             }}
@@ -95,7 +99,7 @@ export function Main (props) {
 
 export default function TemplateModule (props) {
   const { api } = useSubstrate();
-  return api.query.templateModule && api.query.templateModule.cid
+  return api.query.templateModule && api.query.templateModule.vec
     ? <Main {...props} />
     : null;
 }

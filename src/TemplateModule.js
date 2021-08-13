0f49d87c40341be0555131df92122baa082dc41d
samuelburnham <samuel.burnham@gmail.com>
2021-08-13 17:09:10 -0400

    Add sp-im example

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Input, Grid, Statistic } from 'semantic-ui-react';
+import { Form, Input, Grid } from 'semantic-ui-react';
 
 import { useSubstrate } from './substrate-lib';
 import { TxButton } from './substrate-lib/components';
@@ -11,76 +11,23 @@ export function Main (props) {
   // The transaction submission status
   const [status, setStatus] = useState('');
 
-  // The currently stored values
-  const [vec, setVec] = useState('');
-  // const [data, setData] = useState(0);
-
-  // const [currentValue, setCurrentValue] = useState('');
-  const [formValue, setFormValue] = useState('');
-
-  useEffect(() => {
-    let unsubscribe;
-    api.query.templateModule.vec(newValue => {
-      // The storage value is an Option
-      // So we have to check whether it is None first
-      // There is also unwrapOr
-      if (newValue.isNone) {
-        setVec('<None>');
-      } else {
-        setVec(newValue.unwrap().toString());
-      }
-    }).then(unsub => {
-      unsubscribe = unsub;
-    })
-      .catch(console.error);
-
-    return () => unsubscribe && unsubscribe();
-  }, [api.query.templateModule]);
+  const [formValue, setFormValue] = useState(0);
 
   return (
     <Grid.Column width={20} style={{ textAlign: 'center' }}>
       <h1>Vector Storage</h1>
-        <Statistic
-          label='Currently Stored Vector'
-          value={vec}
-          size='mini'
-        />
       <Form>
         <Form.Field>
           <Input
-            label='Vector Input'
-            type='string'
+            label='Input a number'
+            type='number'
             onChange={(_, { value }) => setFormValue(value)}
           />
         </Form.Field>
         <Form.Field style={{ textAlign: 'center' }}>
           <TxButton
             accountPair={accountPair}
-            label='Store Vector'
-            type='SIGNED-TX'
-            setStatus={setStatus}
-            attrs={{
-              palletRpc: 'templateModule',
-              callable: 'storeVector',
-              inputParams: [formValue],
-              paramFields: [true]
-            }}
-          />
-          <TxButton
-            accountPair={accountPair}
-            label='Get Vector'
-            type='SIGNED-TX'
-            setStatus={setStatus}
-            attrs={{
-              palletRpc: 'templateModule',
-              callable: 'retrieveVector',
-              inputParams: [formValue],
-              paramFields: [true]
-            }}
-          />
-          <TxButton
-            accountPair={accountPair}
-            label='Push front'
+            label='Vector Push Front'
             type='SIGNED-TX'
             setStatus={setStatus}
             attrs={{
@@ -99,7 +46,7 @@ export function Main (props) {
 
 export default function TemplateModule (props) {
   const { api } = useSubstrate();
-  return api.query.templateModule && api.query.templateModule.vec
+  return api.query.templateModule && api.query.templateModule.storageVector
     ? <Main {...props} />
     : null;
 }

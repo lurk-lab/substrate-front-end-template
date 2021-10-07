a571d7d356a5105ac130dd26645722849884a9c6
samuelburnham <samuel.burnham@gmail.com>
2021-10-07 16:27:52 -0400

    Revert "Update to latest front end"
    This reverts commit 9c547c9baa858516a6bfd5420f241e6bc92a43fc.

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -1,50 +1,69 @@
-import React, { useState } from 'react';
-import { Form, Input, Grid } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Form, Input, Grid, Card, Statistic } from 'semantic-ui-react';
 
 import { useSubstrate } from './substrate-lib';
 import { TxButton } from './substrate-lib/components';
 
 function Main (props) {
+  const { api } = useSubstrate();
   const { accountPair } = props;
 
   // The transaction submission status
   const [status, setStatus] = useState('');
 
   // The currently stored value
-  const [text, setText] = useState();
+  const [currentValue, setCurrentValue] = useState(0);
+  const [formValue, setFormValue] = useState(0);
 
-  const showFile = async (e) => {
-    e.preventDefault();
-    const reader = new FileReader();
-    reader.onload = async (e) => {
-      const text = (e.target.result);
-      setText(text.toString());
-    };
-    reader.readAsText(e.target.files[0]);
-  };
+  useEffect(() => {
+    let unsubscribe;
+    api.query.templateModule.something(newValue => {
+      // The storage value is an Option<u32>
+      // So we have to check whether it is None first
+      // There is also unwrapOr
+      if (newValue.isNone) {
+        setCurrentValue('<None>');
+      } else {
+        setCurrentValue(newValue.unwrap().toNumber());
+      }
+    }).then(unsub => {
+      unsubscribe = unsub;
+    })
+      .catch(console.error);
+
+    return () => unsubscribe && unsubscribe();
+  }, [api.query.templateModule]);
 
   return (
     <Grid.Column width={8}>
-      <h1>Theorem Prover</h1>
+      <h1>Template Module</h1>
+      <Card centered>
+        <Card.Content textAlign='center'>
+          <Statistic
+            label='Current Value'
+            value={currentValue}
+          />
+        </Card.Content>
+      </Card>
       <Form>
         <Form.Field>
           <Input
-            label='Yatima File'
-            state='newFile'
-            type='file'
-            onChange={(e) => showFile(e)}
+            label='New Value'
+            state='newValue'
+            type='number'
+            onChange={(_, { value }) => setFormValue(value)}
           />
         </Form.Field>
         <Form.Field style={{ textAlign: 'center' }}>
           <TxButton
             accountPair={accountPair}
-            label='Upload'
+            label='Store Something'
             type='SIGNED-TX'
             setStatus={setStatus}
             attrs={{
               palletRpc: 'templateModule',
-              callable: 'theoremProver',
-              inputParams: [text],
+              callable: 'doSomething',
+              inputParams: [formValue],
               paramFields: [true]
             }}
           />
@@ -57,8 +76,7 @@ function Main (props) {
 
 export default function TemplateModule (props) {
   const { api } = useSubstrate();
-  return api.query.templateModule
+  return api.query.templateModule && api.query.templateModule.something
     ? <Main {...props} />
     : null;
 }
-
